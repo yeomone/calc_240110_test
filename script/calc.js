777a9874256e4fd27e5283c1eb05a4373d6eab93
won <won00yeom@naver.com>
2024-01-10 09:48:47 +0900

    체중계 변수 등록

---
+++ script/calc.js
@@ -0,0 +1,6 @@
+let height = document.querySelector('#height')
+let weight = document.querySelector('#weight')
+let goals = document.querySelector('#goals')
+let result = document.querySelector('#result')
+
+console.log(height,weight,goals,result)
